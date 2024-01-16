bbcd6dafd908e7731748e8b0afd004bc581929bd
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-16 14:08:08 +0530

    completed toast and context

--- app/src/main/java/com/example/all_android_basics_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/all_android_basics_tutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.all_android_basics_tutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
+import android.widget.Toast
 import com.example.all_android_basics_tutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        binding.submitOrder.setOnClickListener{
-            val checkMeetRadioButtonId = binding.radioOption.checkedRadioButtonId
-            val meet = findViewById<RadioButton>(checkMeetRadioButtonId)
-            val chese  = binding.chooseChese.isChecked
-            val onion = binding.chooseOnion.isChecked
-            val ordereddetail = "You want to eat with rice \n" + "${meet.text}" + (if(chese) "\nCheese" else "") +
-                    (if(onion) "\n Onion" else "")
-            binding.detailsOfOrder.text = ordereddetail
-        }
+//        binding.submitOrder.setOnClickListener{
+//            val checkMeetRadioButtonId = binding.radioOption.checkedRadioButtonId
+//            val meet = findViewById<RadioButton>(checkMeetRadioButtonId)
+//            val chese  = binding.chooseChese.isChecked
+//            val onion = binding.chooseOnion.isChecked
+//            val ordereddetail = "You want to eat with rice \n" + "${meet.text}" + (if(chese) "\nCheese" else "") +
+//                    (if(onion) "\n Onion" else "")
+//            binding.detailsOfOrder.text = ordereddetail
+//        }
+//        binding.buttonShowToast.setOnClickListener{
+//            Toast.makeText(this , "Hi , I am bharat Thank you for visiting ..." , Toast.LENGTH_LONG).show()
+//        }
 
     }
 }
