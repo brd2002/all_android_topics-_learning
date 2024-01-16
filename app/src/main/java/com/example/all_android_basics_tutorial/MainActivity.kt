0a4cfea8ce811c367d7b2481a9a224b5d78a28b9
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-16 13:54:20 +0530

    completed radio and checkbox button

--- app/src/main/java/com/example/all_android_basics_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/all_android_basics_tutorial/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.all_android_basics_tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import com.example.all_android_basics_tutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        binding.submitOrder.setOnClickListener{
+            val checkMeetRadioButtonId = binding.radioOption.checkedRadioButtonId
+            val meet = findViewById<RadioButton>(checkMeetRadioButtonId)
+            val chese  = binding.chooseChese.isChecked
+            val onion = binding.chooseOnion.isChecked
+            val ordereddetail = "You want to eat with rice \n" + "${meet.text}" + (if(chese) "\nCheese" else "") +
+                    (if(onion) "\n Onion" else "")
+            binding.detailsOfOrder.text = ordereddetail
+        }
+
     }
 }
