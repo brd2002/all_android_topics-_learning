325917dd7c37e542bde2b4f23ab8d78d00b33ae2
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-16 13:05:24 +0530

    this is my first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "all_android_basics_tutorial"
+include(":app")
+ 
